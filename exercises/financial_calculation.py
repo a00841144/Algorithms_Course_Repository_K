9d46e336b6ae73e134a215dd70edae12ea34d464
Kira Dayanna Espinoza Ortiz <a00841144@tec.mx>
2026-08-22 05:37:39 +0000

    Complete Activity 1 Codespaces practice

---
+++ exercises/financial_calculation.py
@@ -0,0 +1,8 @@
+present_value = 500
+interest_rate = 0.10
+periods = 24
+future_value = present_value * (1 + interest_rate) ** periods
+print("Present value:", present_value)
+print("Interest rate:", interest_rate)
+print("Periods:", periods)
+print("Future value:", round(future_value, 2))
---
+++ exercises/hello_world.py
@@ -0,0 +1,4 @@
+student_name = "Kira Dayanna Espinoza OrtiZ"
+print("Hello, World!")
+print("Welcome,", student_name)
+print("Course: Algorithms and Data Analysis")
