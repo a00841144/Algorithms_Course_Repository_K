7c4584675e1067b3e4d6ccb644bb9714fe748220
Kira Dayanna Espinoza Ortiz <a00841144@tec.mx>
2026-08-23 21:18:38 +0000

    Changes requested made

--- exercises/hello_world.py
+++ exercises/hello_world.py
@@ -1,4 +1,4 @@
-team_names = ["Kira Espinoza", "Júlia Monteiro", "Camila Licona"]
+team_names = ["Kira Espinoza", "Júlia Monteiro", "Camila Licona", "Karla Serna"]
 print("Hello World and welcome to the team!")
 print("Course: Algorithms and Data Analysis")
 for name in team_names:
